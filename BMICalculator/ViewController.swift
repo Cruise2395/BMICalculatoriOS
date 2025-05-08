1cc04e3b81e4e42711f7d774bdc0705ccfdb6b7a
Cruise2395 <cruz.wr@icloud.com>
2025-05-08 12:32:24 +0200

    First version

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+   
+    var height = 170
+    var weight = 60
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onWeightChanged(_ sender: UIStepper) {
+        weight = Int(sender.value)
+        weightLabel.text = "\(weight) kg"
+    }
+    @IBAction func onHeightChanged(_ sender: UISlider) {
+        height = Int(sender.value)
+        heightLabel.text = "\(height) cm"
+    }
+    @IBAction func calculate(_ sender: Any) {
+        let result = Float(weight) / pow((Float(height) 100.0), 2)
+        resultLabel.text = String(format: "%2f", result)
+    }
 }
 
